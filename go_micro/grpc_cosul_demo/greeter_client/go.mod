9e4f456f25182af518ec25c60407c612ee2da054
wushaoyu <wusy155@chinaunicom.cn>
2022-10-03 20:17:17 +0800

    修改agent调用

--- go_micro/grpc_cosul_demo/greeter_client/go.mod
+++ go_micro/grpc_cosul_demo/greeter_client/go.mod
@@ -3,23 +3,21 @@ module greeter_client
 go 1.18
 
 require (
-	github.com/mbobakov/grpc-consul-resolver v1.4.4
+	github.com/hashicorp/consul/api v1.3.0
 	google.golang.org/grpc v1.49.0
 	google.golang.org/protobuf v1.28.1
 )
 
 require (
 	github.com/armon/go-metrics v0.3.2 // indirect
-	github.com/go-playground/form v3.1.4+incompatible // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/btree v1.0.0 // indirect
 	github.com/google/go-cmp v0.5.7 // indirect
-	github.com/hashicorp/consul/api v1.3.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
 	github.com/hashicorp/go-immutable-radix v1.1.0 // indirect
 	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/hashicorp/serf v0.8.5 // indirect
-	github.com/jpillora/backoff v1.0.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.1.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
--- go_micro/grpc_cosul_demo/greeter_client/main.go
+++ go_micro/grpc_cosul_demo/greeter_client/main.go
@@ -4,45 +4,46 @@ import (
 	"context"
 	"fmt"
 	"greeter_client/proto/greeter"
+	"strconv"
 
-	_ "github.com/mbobakov/grpc-consul-resolver" // 非常重要
+	"github.com/hashicorp/consul/api"
+
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
 
-	/*
-		//注册consul服务
-		//1.初始化consul服务
-		consulConfig := api.DefaultConfig()
-		// consulConfig.Address = "127.0.0.1:8500"  consul默认地址
+	//注册consul服务
+	//1.初始化consul服务
+	consulConfig := api.DefaultConfig()
+	// consulConfig.Address = "127.0.0.1:8500"  consul默认地址
 
-		//2.获取consul操作对象
-		consulClient, _ := api.NewClient(consulConfig)
+	//2.获取consul操作对象
+	consulClient, _ := api.NewClient(consulConfig)
 
-		//3.获取地址
-		serviceEntry, _, _ := consulClient.Health().Service("HelloService", "test", false, nil)
-		fmt.Println(serviceEntry[0].Service.Address)
-		fmt.Println(serviceEntry[0].Service.Port)
+	//3.获取地址
+	serviceEntry, _, _ := consulClient.Health().Service("HelloService", "test", false, nil)
+	fmt.Println(serviceEntry[0].Service.Address)
+	fmt.Println(serviceEntry[0].Service.Port)
 
-		address := serviceEntry[0].Service.Address + ":" + strconv.Itoa(serviceEntry[0].Service.Port)
-		//grpc客户端
-		//连接服务器
-		grpcClient, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
-		if err != nil {
-			fmt.Printf("连接服务端失败:%s", err)
-		}
-
-	*/
-
-	grpcClient, err := grpc.Dial(
-		"consul://127.0.0.1:8500/HelloService",
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`))
+	address := serviceEntry[0].Service.Address + ":" + strconv.Itoa(serviceEntry[0].Service.Port)
+	//grpc客户端
+	//连接服务器
+	grpcClient, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		fmt.Printf("grpc client连接报错:%v", err)
+		fmt.Printf("连接服务端失败:%s", err)
 	}
+
+	/*
+		grpcClient, err := grpc.Dial(
+			"consul://127.0.0.1:8500/HelloService",
+			grpc.WithTransportCredentials(insecure.NewCredentials()),
+			grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy": "round_robin"}`))
+		if err != nil {
+			fmt.Printf("grpc client连接报错:%v", err)
+		}
+	*/
 	//2.注册客户端
 	client := greeter.NewGreeterClient(grpcClient)
 
